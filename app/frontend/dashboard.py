e55a1ad92a280ce276000b49864a04d45ec458b7
Danil Zhuravlov <danil.zhuravlov.2004@gmail.com>
2025-01-24 13:44:47 +0100

    created dashboard.py that will store streamlit code that is needed to show analytics using plotly.

---
+++ app/frontend/dashboard.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(
+    page_title="Slimr Analytics",
+    page_icon="📊",
+    layout='wide'
+)
+
+st.title("📊 Slimr Analytics Dashboard")
